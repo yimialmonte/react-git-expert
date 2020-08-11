1a7ed0ffbd1ed3b0195811a4c883709269c0df3f
yimialmonte <yelmialmonte@gmail.com>
2020-08-11 17:21:10 -0400

    create a component for the gif grid

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,25 +1,18 @@
 import React, { useState } from 'react';
 import AddCategory from './components/CategoyApp';
+import { GifGrid } from './components/GifGrid';
 
 const GifExpertApp = () => {
-  const [categories, setCategories] = useState([
-    'One Punch',
-    'Samuarai',
-    'Dragon Ball',
-  ]);
-
-  const hanldedAdd = (newOne) => {
-    setCategories(prev => [...prev, newOne])
-  }
+  const [categories, setCategories] = useState(['One Punch']);
 
   return (
     <>
       <h2>Gif Expert App</h2>
-      <AddCategory setCategories={setCategories}/>
+      <AddCategory setCategories={setCategories} />
       <hr />
       <ol>
-        {categories.map((item, index) => (
-          <li key={index}>{item}</li>
+        {categories.map((category) => (
+          <GifGrid key={category} category={category} />
         ))}
       </ol>
     </>
---
+++ src/components/GifGrid.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const GifGrid = ({ category }) => {
+  console.log(category)
+  return (
+    <div>
+      <h3>{category}</h3>
+    </div>
+  )
+}
