0f64437061c96f34c6317f56e549655618c346eb
yimialmonte <yelmialmonte@gmail.com>
2020-08-13 05:59:21 -0400

    added docs folder for deployment

---
+++ docs/precache-manifest.e294090b5de7690e33aa1b198c406829.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d45219dd5bf778a15c54b2655103777",
+    "url": "/index.html"
+  },
+  {
+    "revision": "248423f6dd6ead7ffd10",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "fd37ee5a9a93a84ff827",
+    "url": "/static/css/main.e8ad409f.chunk.css"
+  },
+  {
+    "revision": "248423f6dd6ead7ffd10",
+    "url": "/static/js/2.1a68ba2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1a68ba2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd37ee5a9a93a84ff827",
+    "url": "/static/js/main.1e5f25f6.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
